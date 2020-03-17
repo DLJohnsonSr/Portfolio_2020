96fac6902b6a505213d61fd518de5b73420c750e
dljohnsonsr <dljsrdev@gmail.com>
2020-03-17 13:45:16 -0400

    first mobile draft

--- public/js/components.js
+++ public/js/components.js
@@ -8,10 +8,12 @@ const body = document.body;
 const navMenuIcon = document.getElementById("nav-menu-icon");
 const navList = document.getElementById("nav-menu-list");
 
+// toggle menu list open or closed on navbar
 navMenuIcon.addEventListener("click", ()=>{
     navList.classList.toggle("menu-display");
 });
 
-navList.addEventListener("click", ()=>{
-    navList.classList.remove("menu-display");
-});
+// close menu list when menu link clicked
+// navList.addEventListener("click", ()=>{
+//     navList.classList.remove("menu-display");
+// });
